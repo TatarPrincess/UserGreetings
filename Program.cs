d5e6db370d3d7a320d8e79b8d43deafe0948b80f
TatarPrincess <TatarPrincessP@gmail.com>
2022-12-09 09:43:25 +0300

    First commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace UsersGreeting
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var userList = new UserList();
+
+            foreach (User user in userList.users)
+            {
+                Greeting.Greet(user.Name);
+                if (!user.IsPremium) Greeting.ShowAds(); 
+            }
+        }
+        
+    }
+}
---
+++ UserList.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsersGreeting
+{
+    public class UserList
+    {
+        public List <User> users;
+
+        public UserList()
+        { 
+          users= new List<User>();
+          users.Add(new User("Tanya", "Tanya Kim", true));
+          users.Add(new User("Sasha", "Sasha Kim", false));
+          users.Add(new User("Igor", "Igor Kim", false));
+        }        
+    }
+}
